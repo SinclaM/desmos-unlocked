9fde9742fa74170aa6a4e3ac4557737652d67b4a
SinclaM <matthewsinclair2024@u.northwestern.edu>
2022-06-04 14:08:49 -0500

    Fix bug in background.js, add header to popup

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ browser.runtime.onInstalled.addListener(function () {
     browser.storage.local.set(
         {
             autoCommands:
-                "alpha beta sqrt theta Theta phi Phi pi Pi tau nthroot cbrt sum prod int ans percent infinity infty gamma Gamma delta Delta epsilon epsiv zeta eta kappa lambda Lambda mu xi Xi rho sigma Sigma chi Psi omega Omega",
+                "alpha beta sqrt theta Theta phi Phi pi Pi tau nthroot cbrt sum prod int ans percent infinity infty gamma Gamma delta Delta epsilon epsiv zeta eta kappa lambda Lambda mu xi Xi rho sigma Sigma chi Psi omega Omega digamma iota nu upsilon Upsilon psi Psi square mid parallel nparallel perp to times div approx",
         },
         function () {}
     );
