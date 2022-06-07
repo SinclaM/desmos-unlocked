ba79a7edf216f39dcf804377eb17e62f81790073
SinclaM <matthewsinclair2024@u.northwestern.edu>
2022-06-07 18:18:30 -0500

    Run prettier on codebase

--- src/test/app.spec.ts
+++ src/test/app.spec.ts
@@ -3,23 +3,23 @@ import { App } from '../lib/app';
 import * as Dom from '../lib/dom';
 
 describe('testing App', () => {
-  let sandboxes;
-  let renderDateTime;
+    let sandboxes;
+    let renderDateTime;
 
-  beforeEach(() => {
-    sandboxes = sinon.createSandbox();
-    renderDateTime = sandboxes.stub(Dom, 'renderDateTime');
-  });
-
-  afterEach(() => {
-    sandboxes.restore();
-  });
-
-  describe('testing App', () => {
-    it('should return Thu Nov 19 23:00', () => {
-      const app = new App();
-      app.render();
-      expect(renderDateTime.called).toBeTruthy();
+    beforeEach(() => {
+        sandboxes = sinon.createSandbox();
+        renderDateTime = sandboxes.stub(Dom, 'renderDateTime');
     });
-  });
+
+    afterEach(() => {
+        sandboxes.restore();
+    });
+
+    describe('testing App', () => {
+        it('should return Thu Nov 19 23:00', () => {
+            const app = new App();
+            app.render();
+            expect(renderDateTime.called).toBeTruthy();
+        });
+    });
 });
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -3,22 +3,22 @@ const TerserPlugin = require('terser-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-  mode: 'production',
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        /*
-         * Google requires some conditions:
-         * - Removal of whitespace, newlines, code comments, and block delimiters
-         * - Shortening of variable and function names
-         * - Collapsing the number of JavaScript files
-         */
-        terserOptions: {
-          compress: true, // To rename variables & function names
-          mangle: true, // Note `mangle.properties` is `false` by default.
-        },
-      }),
-    ],
-  },
+    mode: 'production',
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                /*
+                 * Google requires some conditions:
+                 * - Removal of whitespace, newlines, code comments, and block delimiters
+                 * - Shortening of variable and function names
+                 * - Collapsing the number of JavaScript files
+                 */
+                terserOptions: {
+                    compress: true, // To rename variables & function names
+                    mangle: true, // Note `mangle.properties` is `false` by default.
+                },
+            }),
+        ],
+    },
 });
