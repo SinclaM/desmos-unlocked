fb630795407d78e0fe9e69cc66c7ff71fc165e29
SinclaM <matthewsinclair2024@u.northwestern.edu>
2022-06-13 22:53:29 -0500

    Add Opera support

---
+++ src/background/opera/background.ts
@@ -0,0 +1,20 @@
+/// <reference types="chrome"/>
+
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.local.set({
+        // keepmeKEEPME should always remain in autoCommands, since MathQuill requires a nonempty
+        // space-delimited list of commands.
+        autoCommands:
+            'keepmeKEEPME alpha beta sqrt theta Theta phi Phi pi Pi tau nthroot cbrt sum prod int ans percent infinity infty gamma Gamma delta Delta epsilon epsiv zeta eta kappa lambda Lambda mu xi Xi rho sigma Sigma chi Psi omega Omega digamma iota nu upsilon Upsilon psi Psi square mid parallel nparallel perp to times div approx',
+    });
+
+    // Make extension work on all pages
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+        chrome.declarativeContent.onPageChanged.addRules([
+            {
+                conditions: [new chrome.declarativeContent.PageStateMatcher({})],
+                actions: [new chrome.declarativeContent.ShowPageAction()],
+            },
+        ]);
+    });
+});
