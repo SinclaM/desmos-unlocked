c967591dd4cf9caa0c4ea8fb88605ba454720d0c
SinclaM <matthewsinclair2024@u.northwestern.edu>
2022-06-14 16:50:14 -0500

    Update permissions

--- src/background/chrome/background.ts
+++ src/background/chrome/background.ts
@@ -7,14 +7,4 @@ chrome.runtime.onInstalled.addListener(function () {
         autoCommands:
             'keepmeKEEPME alpha beta sqrt theta Theta phi Phi pi Pi tau nthroot cbrt sum prod int ans percent infinity infty gamma Gamma delta Delta epsilon epsiv zeta eta kappa lambda Lambda mu xi Xi rho sigma Sigma chi Psi omega Omega digamma iota nu upsilon Upsilon Psi square mid parallel nparallel perp to times div approx',
     });
-
-    // Make extension work on all pages
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-        chrome.declarativeContent.onPageChanged.addRules([
-            {
-                conditions: [new chrome.declarativeContent.PageStateMatcher({})],
-                actions: [new chrome.declarativeContent.ShowPageAction()],
-            },
-        ]);
-    });
 });
