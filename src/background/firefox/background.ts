f4a83cafeada17fadf4df7b1ee712194ce723c99
SinclaM <matthewsinclair2024@u.northwestern.edu>
2022-07-18 21:56:17 -0500

    Remove shortcut 'to' (→) from defaults

--- src/background/firefox/background.ts
+++ src/background/firefox/background.ts
@@ -5,7 +5,7 @@ browser.runtime.onInstalled.addListener(function () {
         // keepmeKEEPME should always remain in autoCommands, since MathQuill requires a nonempty
         // space-delimited list of commands.
         autoCommands:
-            "keepmeKEEPME alpha beta sqrt theta Theta phi Phi pi Pi tau nthroot cbrt sum prod int ans percent infinity infty gamma Gamma delta Delta epsilon epsiv zeta eta kappa lambda Lambda mu xi Xi rho sigma Sigma chi Psi omega Omega digamma iota nu upsilon Upsilon Psi square mid parallel nparallel perp to times div approx",
+            "keepmeKEEPME alpha beta sqrt theta Theta phi Phi pi Pi tau nthroot cbrt sum prod int ans percent infinity infty gamma Gamma delta Delta epsilon epsiv zeta eta kappa lambda Lambda mu xi Xi rho sigma Sigma chi Psi omega Omega digamma iota nu upsilon Upsilon Psi square mid parallel nparallel perp times div approx",
     });
 });
 
