c1891b24c7ccffce3d35932dcf9830129aa44625
SinclaM <matthewsinclair2024@u.northwestern.edu>
2022-07-22 15:25:22 -0500

    Allow enabling/disabling all extended shorcuts, and refactor

--- src/popup.ts
+++ src/popup.ts
@@ -3,6 +3,100 @@ import { massSet, storeConfig, populateGrid, toggleConfig } from "./utils/utils"
 import extendedShortcuts from "./utils/extendedShortcuts";
 
 document.addEventListener("DOMContentLoaded", async () => {
+    const setToDefaultCommon = () => {
+        if (autoParen.checked) {
+            autoParen.click();
+        }
+        if (subscriptShortcuts.checked) {
+            subscriptShortcuts.click();
+        }
+        disableAll.click();
+        if (extendSymbols.checked) {
+            extendSymbols.click();
+        }
+        breakoutChars.value = "+-=<>*";
+        setChars.click();
+    };
+
+    document.getElementById("set-to-default").onclick = function () {
+        massSet(
+            Array.from(document.querySelectorAll("#desmos-default .latex-item, #basic .latex-item"))
+                .map((item) => item.id)
+                .filter(function (item) {
+                    return !new Set(["ge", "le", "ne", "pm", "mp", "psi", "to"]).has(item);
+                }),
+            "autoCommands"
+        );
+        setToDefaultCommon();
+    };
+
+    document.getElementById("set-to-desmos-default").onclick = function () {
+        massSet(
+            Array.from(document.querySelectorAll("#desmos-default .latex-item")).map((item) => item.id),
+            "autoCommands"
+        );
+        setToDefaultCommon();
+    };
+
+    // Add all the dynamically loaded nodes to the DOM using templates and give
+    // sliders their funcionality
+    const { autoCommands }: { autoCommands: string } = await browser.storage.local.get("autoCommands");
+    populateGrid("grid-item-template", "desmos-default", desmosDefualtAutoCommands, autoCommands);
+    populateGrid("grid-item-template", "basic", basicAutoCommands, autoCommands);
+    populateGrid("grid-item-template", "advanced", advancedAutoCommands, autoCommands);
+
+    // Make the sliders actually update user configs when clicked
+    document.querySelectorAll(".latex-item").forEach(function (item) {
+        item.querySelector(".onoffswitch-checkbox").addEventListener("click", storeConfig);
+    });
+
+    const extendSymbols = document.getElementById("switch-extend-mq") as HTMLInputElement;
+    const collapsible = document.getElementById("collapsible");
+
+    extendSymbols.addEventListener("click", () => {
+        toggleConfig("enableMathquillOverrides");
+        collapsible.style.display = collapsible.style.display === "none" ? "block" : "none";
+    });
+
+    browser.storage.local.get("enableMathquillOverrides").then((stored) => {
+        extendSymbols.checked = stored.enableMathquillOverrides as boolean;
+        collapsible.style.display = extendSymbols.checked ? "block" : "none";
+    });
+
+    const extendedText = document.querySelector<HTMLInputElement>("#collapsible textarea");
+    extendedText.value = autoCommands
+        .split(" ")
+        .filter((shortcut) => extendedShortcuts.has(shortcut))
+        .join(" ");
+
+    const setExtendedText = document.querySelector<HTMLInputElement>("#collapsible button.set");
+    setExtendedText.onclick = async () => {
+        try {
+            validateCommands(extendedText.value);
+            const { autoCommands }: { autoCommands: string } = await browser.storage.local.get("autoCommands");
+            const baseCommands = autoCommands
+                .split(" ")
+                .filter((shortcut) => !extendedShortcuts.has(shortcut))
+                .join(" ");
+            const extendedCommands = extendedText.value === "" ? "" : " " + extendedText.value;
+            browser.storage.local.set({ autoCommands: baseCommands + extendedCommands });
+            document.getElementById("error-bar").textContent = "";
+        } catch (e) {
+            document.getElementById("error-bar").textContent = e;
+        }
+    };
+
+    const enableAll = document.querySelector<HTMLInputElement>("#collapsible button.enable");
+    enableAll.onclick = () => {
+        extendedText.value = [...extendedShortcuts].join(" ");
+        setExtendedText.click();
+    };
+    const disableAll = document.querySelector<HTMLInputElement>("#collapsible button.disable");
+    disableAll.onclick = () => {
+        extendedText.value = "";
+        setExtendedText.click();
+    };
+
     const breakoutChars = document.querySelector<HTMLInputElement>("#breakout textarea");
     const setChars = document.getElementById("set-chars");
 
@@ -30,103 +124,19 @@ document.addEventListener("DOMContentLoaded", async () => {
         // (disableAutoSubstitutionInSubscripts === true means shortcuts disabled).
         subscriptShortcuts.checked = !stored.disableAutoSubstitutionInSubscripts as boolean;
     });
-
-    const extendSymbols = document.getElementById("switch-extend-mq") as HTMLInputElement;
-    const collapsible = document.getElementById("collapsible");
-
-    extendSymbols.addEventListener("click", () => {
-        toggleConfig("enableMathquillOverrides");
-        collapsible.style.display = collapsible.style.display === "none" ? "block" : "none";
-    });
-
-    browser.storage.local.get("enableMathquillOverrides").then((stored) => {
-        extendSymbols.checked = stored.enableMathquillOverrides as boolean;
-        collapsible.style.display = extendSymbols.checked ? "block" : "none";
-    });
-
-    const setToDefault = document.getElementById("set-to-default");
-    const setToDesmosDefault = document.getElementById("set-to-desmos-default");
-
-    const setToDefaultCommon = () => {
-        if (autoParen.checked) {
-            autoParen.click();
-        }
-        if (subscriptShortcuts.checked) {
-            subscriptShortcuts.click();
-        }
-        if (extendSymbols.checked) {
-            extendSymbols.click();
-        }
-        breakoutChars.value = "+-=<>*";
-        setChars.click();
-    };
-
-    setToDefault.onclick = function () {
-        massSet(
-            Array.from(document.querySelectorAll("#desmos-default .latex-item, #basic .latex-item"))
-                .map((item) => item.id)
-                .filter(function (item) {
-                    return item !== "ge" && item !== "le" && item !== "ne" && item !== "pm" && item !== "mp";
-                }),
-            "autoCommands"
-        );
-        setToDefaultCommon();
-    };
-
-    setToDesmosDefault.onclick = function () {
-        massSet(
-            Array.from(document.querySelectorAll("#desmos-default .latex-item")).map((item) => item.id),
-            "autoCommands"
-        );
-        setToDefaultCommon();
-    };
-
-    // Add all the dynamically loaded nodes to the DOM using templates and give
-    // sliders their funcionality
-    const { autoCommands }: { autoCommands: string } = await browser.storage.local.get("autoCommands");
-    populateGrid("grid-item-template", "desmos-default", desmosDefualtAutoCommands, autoCommands);
-    populateGrid("grid-item-template", "basic", basicAutoCommands, autoCommands);
-    populateGrid("grid-item-template", "advanced", advancedAutoCommands, autoCommands);
-
-    // Make the sliders actually update user configs when clicked
-    document.querySelectorAll(".latex-item").forEach(function (item) {
-        item.querySelector(".onoffswitch-checkbox").addEventListener("click", storeConfig);
-    });
-
-    const extendedText = document.querySelector<HTMLInputElement>("#collapsible textarea");
-    extendedText.value = autoCommands
-        .split(" ")
-        .filter((shortcut) => extendedShortcuts.has(shortcut))
-        .join(" ");
-
-    const validateCommands = (commands: string) => {
-        // The empty string is valid
-        if (commands === "") {
-            return;
-        }
-        if (typeof commands !== "string" || !/^[a-z]+(?: [a-z]+)*$/i.test(commands)) {
-            throw "🚫 not a space-delimited list of only letters";
-        }
-        commands.split(" ").forEach((command) => {
-            if (!extendedShortcuts.has(command)) {
-                throw `🚫 ${command} is not a valid extended shortcut`;
-            }
-        });
-    };
-    const setExtendedText = document.querySelector<HTMLInputElement>("#collapsible button.set");
-    setExtendedText.onclick = async () => {
-        try {
-            validateCommands(extendedText.value);
-            const { autoCommands }: { autoCommands: string } = await browser.storage.local.get("autoCommands");
-            const baseCommands = autoCommands
-                .split(" ")
-                .filter((shortcut) => !extendedShortcuts.has(shortcut))
-                .join(" ");
-            const extendedCommands = extendedText.value === "" ? "" : " " + extendedText.value;
-            browser.storage.local.set({ autoCommands: baseCommands + extendedCommands });
-            document.getElementById("error-bar").textContent = "";
-        } catch (e) {
-            document.getElementById("error-bar").textContent = e;
-        }
-    };
 });
+
+function validateCommands(commands: string) {
+    // The empty string is valid
+    if (commands === "") {
+        return;
+    }
+    if (typeof commands !== "string" || !/^[a-z]+(?: [a-z]+)*$/i.test(commands)) {
+        throw "🚫 not a space-delimited list of only letters";
+    }
+    commands.split(" ").forEach((command) => {
+        if (!extendedShortcuts.has(command)) {
+            throw `🚫 ${command} is not a valid extended shortcut`;
+        }
+    });
+}
