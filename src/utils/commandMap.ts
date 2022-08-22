9d4949d66ea3b7ab9f76f0802e2a18b65e1d0a8e
SinclaM <matthewsinclair2024@u.northwestern.edu>
2022-08-22 09:54:44 -0400

    Run prettier

--- src/utils/commandMap.ts
+++ src/utils/commandMap.ts
@@ -4,14 +4,14 @@ interface CommandMap {
 
 interface Replacement {
     // The name to be used in the mock LaTeX command. Also acts as the name Desmos
-    // uses when giving error messages. This is not necessarily the actual shortcut 
+    // uses when giving error messages. This is not necessarily the actual shortcut
     // recognized by MathQuill, although in practice it is often the same.
     cmdName: string;
     // Alt code for desired unicode symbol. In decimal, not hex.
     altCode: string;
     // A more detailed description, although in practice very similar to `shortcut`.
     // Not sure if/where this would appear in Desmos.
-    description?: string; 
+    description?: string;
 }
 
 type bindFunction = (latex: string, mqAltCode: string, description: string) => any; // FIXME
@@ -28,6 +28,6 @@ export function remap(cmds: { [index: string]: any }, replacements: CommandMap, 
 }
 
 export const emojis: CommandMap = {
-    "burger": {cmdName: "burger", altCode: "127828", description: "burger"},
-    "hotdog": {cmdName: "hotdog", altCode: "127789", description: "Hot dog"}
+    burger: { cmdName: "burger", altCode: "127828", description: "burger" },
+    hotdog: { cmdName: "hotdog", altCode: "127789", description: "Hot dog" },
 };
